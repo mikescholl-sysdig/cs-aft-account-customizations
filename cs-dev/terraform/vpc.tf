2356a402147ef1b1dae64e50d4e28adb7856e547
Michael Scholl <michael.scholl@sysdig.com>
2022-06-15 20:55:46 -0700

    Adding customization to CS Dev accounts

---
+++ cs-dev/terraform/vpc.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    name = "${data.aws_iam_account_alias.current.account_alias}-vpc"
+    cidr = "10.0.0.0/16"
+    azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+    private_subnets = ["10.0.64.0/24", "10.0.65.0/24", "10.0.66.0/24"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    database_subnets = ["10.0.192.0/24", "10.0.193.0/24", "10.0.194.0/24"]
+    create_database_subnet_route_table = true
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_vpn_gateway = false
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    enable_flow_log = false    
+    tags = {
+        Terraform = "true"
+    }
+}
